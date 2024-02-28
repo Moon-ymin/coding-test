6f7a15f42c147c13b5da2aa88adbe59468f9b32c
Moon-ymin <ansdudals8@naver.com>
2024-02-28 10:02:26 +0900

    2월 28일 그리디 알고리즘

--- src/greedy/part3/Ex4.java
+++ src/greedy/part3/Ex4.java
@@ -1,8 +1,31 @@
 package greedy.part3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ex4 {
     // 만들 수 없는 금액
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> coins = new ArrayList<>();
+        String money = sc.next();
 
+        for (int i=0; i<n; i++) {
+            coins.add(  money.charAt(i) - '0' );
+        }
+
+        Collections.sort(coins);
+
+        int target = 1;
+        for (int coin : coins){
+            if ( coin > target ){
+                break;
+            } else {
+                target += coin;
+            }
+        }
+        System.out.println(target);
     }
 }
