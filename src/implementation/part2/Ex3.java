933f5d769913634d30f8b249f06d3df728ca95cb
MoonYoungMin <ansdudals8@naver.com>
2024-03-01 23:49:56 +0900

    3월 1일 구현 part2_Ex3 수정

--- src/implementation/part2/Ex3.java
+++ src/implementation/part2/Ex3.java
@@ -4,62 +4,73 @@ import java.util.Scanner;
 
 public class Ex3 {
     // 게임 개발
+
     static int[] dx = {-1, 0, 1, 0}; // 북 동 남 서
     static int[] dy = {0, 1, 0, -1};
+    static int dir; // 게임 캐릭터가 바라보는 방향
 
-    static int turnLeft( int dir ) { // 왼쪽 방향
+    static void turnLeft() { // 왼쪽 방향으로 회전
         dir -= 1;
         if (dir == -1) {
             dir = 3;
         }
-        return dir;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();       // 맵 생성
         int M = sc.nextInt();
         int[][] map= new int[N][M];
-        int[][] visited = new int[N][M];
+        int[][] visit = new int[N][M];
 
         int x = sc.nextInt();       // 현재 위치, 방향
         int y = sc.nextInt();
-        int dir = sc.nextInt();
-        visited[x][y] = 1;
+        dir = sc.nextInt();
+        visit[x][y] = 1;
+        int visit_cnt = 1; // 결과 - 방문한 칸
+        int cnt = 0;    // 네 방향 확인
 
         for(int i=0; i<N; i++) {    // 맵 채우기
             for(int j=0; j<M; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
-        int cnt = 0;
-        int fail = 0;
-        while (true){
-            dir = turnLeft(dir); // 왼쪽 회전
-            fail++;
-            int nextx = 0, nexty = 0;
-            nextx = x + dx[dir];      // 다음 위치
-            nexty = y + dy[dir];
 
-            // 육지(0)이고, 가본 적 없는 곳
-            if ( map[nextx][nexty] == 0 && visited[nextx][nexty] == 0) {
+        while (true){
+            turnLeft(); // 1. 왼쪽 회전
+            int nextx = x + dx[dir];      // 다음 위치
+            int nexty = y + dy[dir];
+
+            // 2. 육지(0)이고, 가본 적 없는 곳
+            if ( map[nextx][nexty] == 0 && visit[nextx][nexty] == 0) {
                 x = nextx;  // 이동
                 y = nexty;
-                visited[x][y] = 1; // 가봤다고 표시
-                cnt++;
-                fail = 0;
+                visit[x][y] = 1; // 가봤다고 표시
+                visit_cnt += 1;
+                cnt = 0;
+                continue;
+            } else {
+                cnt += 1; // 회전했는데, 가보지 않은 칸이 없을 때
             }
-            if ( fail == 4 ){   // 네 번 다 회전해본거임
+
+            // 3. 네 번 다 회전함
+            if ( cnt == 4 ){
                 // 방향 유지, 뒤로 이동
                 nextx = x - dx[dir];
                 nexty = y - dy[dir];
 
-                if (map[nextx][nexty] == 1) break; // 바다일 경우
-                else x = nextx; y = nexty; visited[x][y] = 1; fail = 0;
+                if (map[nextx][nexty] == 0){
+                    // 가능하다면 한 칸 뒤로
+                    x = nextx; y = nexty; cnt = 0;
+                } else {
+                  // 한 칸 뒤로 못 가면 움직임 멈춤
+                  break;
+                }
             }
 
         }
 
-        System.out.println(cnt);
+        System.out.println(visit_cnt);
     }
 
 
