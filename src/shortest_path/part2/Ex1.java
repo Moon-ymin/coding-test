8dc535edd7c62d0b28c99c766edec6a51f76efe6
Moon-ymin <ansdudals8@naver.com>
2024-03-25 12:51:38 +0900

    3월 25일 | 최단 경로 part2

---
+++ src/shortest_path/part2/Ex1.java
@@ -0,0 +1,13 @@
+package shortest_path.part2;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+    }
+}
