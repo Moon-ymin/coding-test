e5465788ce91dbcf7fdab8e2cbd725a7fbaafa18
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-05-15 16:13:00 +0900

    sc.nextLine() occured memory error

--- src/swea/swea1288.java
+++ src/swea/swea1288.java
@@ -1,5 +1,58 @@
+// 1번 코드
 import java.util.*;
 
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+       
+
+        for (int testcase = 1; testcase <= T ; testcase++) {
+            int N = sc.nextInt();
+            sc.nextLine();
+            Set<Integer> nums = new TreeSet<>();
+
+            int mul = 1;
+            while(nums.size() < 10) {
+                String[] num = String.valueOf(N * mul++).split("");
+                for (int i = 0; i < num.length; i++) {
+                    nums.add(Integer.parseInt(num[i]));
+                }
+            }
+            System.out.println("#" + testcase + " " + (N * --mul));
+        }
+
+    }
+}
+
+
+// 2번 코드
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int testcase = 1; testcase <= T ; testcase++) {
+            int N = sc.nextInt();
+            Set<Integer> nums = new TreeSet<>();
+
+            int mul = 1;
+            while(nums.size() < 10) {
+                String[] num = String.valueOf(N * mul++).split("");
+                for (int i = 0; i < num.length; i++) {
+                    nums.add(Integer.parseInt(num[i]));
+                }
+            }
+            System.out.println("#" + testcase + " " + (N * --mul));
+        }
+
+    }
+}
+
+
+// 3번 코드 
 public class Solution {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -21,3 +74,6 @@ public class Solution {
 
     }
 }
+
+
+// 있을 때 1번 코드만 메모리 오류가 떴고, 2번 3번은 메모리 오류 안떴어 왜그런거야?
