e63e1e306292c7d7f253bd909637f724be95bc71
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-08-27 21:59:55 +0900

    [Gold IV] Title: 알파벳, Time: 2376 ms, Memory: 300356 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -18,13 +18,13 @@ public class Main {
         map = new String[R][C];
         chars = new HashSet<>();
 
-        for (int r = 0; r < R; r++) {
-            String str = sc.next();
-            for (int c = 0; c < C; c++) {
-                map[r][c] = "" + str.charAt(c);
+        for (int i = 0; i < R; i++) {
+            String s = sc.next();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = "" + s.charAt(j);
             }
         }
-        
+
         max = 0;
         chars.add(map[0][0].charAt(0));
         isVisited = new boolean[R][C];
@@ -32,21 +32,21 @@ public class Main {
         dfs(0, 0, 1);
         System.out.println(max);
     }
-
     private static void dfs(int x, int y, int depth) {
         max = Math.max(max, depth);
 
         for (int dir = 0; dir < 4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
             char nextChar = map[nx][ny].charAt(0);
             if (isVisited[nx][ny] || chars.contains(nextChar)) continue;
 
             isVisited[nx][ny] = true;
             chars.add(nextChar);
             dfs(nx, ny, depth + 1);
-            chars.remove(nextChar);
+            chars.remove(nextChar); // 맨 끝에 고른애 빼고 다른 경로 생각하기 - 백트래킹
             isVisited[nx][ny] = false;
         }
     }
