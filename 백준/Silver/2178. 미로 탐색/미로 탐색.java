4d416f668c648eaea7f319fe554e6afab9263356
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-08-31 23:19:14 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 11992 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,58 +1,59 @@
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
     static int[][] map;
     static boolean[][] isVisited;
-    static int N, M;
+    static int N, M, min;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        N = sc.nextInt();
-        M = sc.nextInt();
-        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
         isVisited = new boolean[N][M];
 
-        for (int i = 0; i < N; i++) {
-            String s = sc.next();
-            for (int j = 0; j < M; j++) {
+        for(int i = 0; i < N; i++){
+            String s = br.readLine();
+            for(int j = 0; j < M; j++){
                 map[i][j] = s.charAt(j) - '0';
             }
         }
-
+        min = Integer.MAX_VALUE;
         System.out.println(bfs(0, 0));
     }
 
-    private static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y});
+    private static int bfs(int x, int y){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{x, y});
         isVisited[x][y] = true;
-        map[x][y] = 1; // 시작 지점에서의 거리
+        map[x][y] = 1; // 시작 지점에서 거리 1로 시작
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int cx = current[0];
-            int cy = current[1];
-            
-            // 목표 지점에 도착
-            if (cx == N - 1 && cy == M - 1) {
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0], cy = cur[1];
+
+            // 목표 지점 도달했으면
+            if (cx == N-1 && cy == M-1){
                 return map[cx][cy];
             }
-            
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cx + dx[dir];
-                int ny = cy + dy[dir];
-                
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == 1 && !isVisited[nx][ny]) {
-                    isVisited[nx][ny] = true;
-                    map[nx][ny] = map[cx][cy] + 1; // 거리 누적
-                    queue.add(new int[] {nx, ny});
-                }
+
+            for(int dir = 0; dir < 4; dir++){
+                int nx = cx + dx[dir], ny = cy + dy[dir];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny]==0 || isVisited[nx][ny]) continue;
+                isVisited[nx][ny] = true;
+                map[nx][ny] = map[cx][cy] + 1;
+                q.add(new int[]{nx, ny});
             }
         }
         return -1;
