88ace0474e131b439a1802989513b0485c8b52ff
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-09-03 21:49:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 153 ms, Memory: 26,256 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,125 +1,113 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Node {
-	public int r,c;
+public class Solution {
+    static int N, M, R, C, L, map[][], result;
+    static boolean[][] isVisited;
 
-	public Node(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
+    // 방향 설정 (상, 하, 좌, 우)
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-	public int getR() {
-		return r;
-	}
+    // 파이프 종류별로 연결 가능한 방향들 (상, 하, 좌, 우)
+    static int[][] pipeDir = {
+            {},
+            {0, 1, 2, 3},  // 1번 파이프
+            {0, 1},        // 2번 파이프
+            {2, 3},        // 3번 파이프
+            {0, 3},        // 4번 파이프
+            {1, 3},        // 5번 파이프
+            {1, 2},        // 6번 파이프
+            {0, 2}         // 7번 파이프
+    };
 
-	public int getC() {
-		return c;
-	}
+    static class Pipe {
+        int x, y;
 
-}
+        public Pipe(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-	static int n,m,sr,sc,l;
-	static int[][] graph;
-	static boolean[][] visited;
-	static int answer;
-	static int t;
-	//상우하좌
-	static int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
-	static int[][] haveDir = {
-			{},//0
-			{0,1,2,3},//1
-			{0,2},//2
-			{1,3},//3
-			{0,1},//4
-			{1,2},//5
-			{2,3},//6
-			{0,3}//7
-	};
-	
-	static boolean canGo(int r, int c, int direct) {
-		int curDir = graph[r][c];
-		for(int d : haveDir[curDir]) {
-			if(d==direct) 
-				return true;
-		}
-		return false;
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new LinkedList<Node>();
-		q.add(new Node(sr,sc));
-		visited[sr][sc] = true;
-		int time=1;
-		
-		if(time==l) return;
-		
-		while(!q.isEmpty()) {
-			int size = q.size();
-			
-			for(int s =0; s<size; s++) {
-				Node cur = q.poll();
-				int curTunnel = graph[cur.r][cur.c];
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            map = new int[N][M];
 
-				for(int i=0; i<haveDir[curTunnel].length;i++) {
-					int curDir = haveDir[curTunnel][i];
-					int nr = cur.r + dir[curDir][0];
-					int nc = cur.c + dir[curDir][1];
-				
-					if(nr<0 || nr>=n || nc<0 || nc>=m) continue;
-					if(graph[nr][nc]==0) continue;
-					//다음 노드가 반대 방향 가지고 있으면 canGo == true 
-					if(!canGo(nr,nc,(curDir+2)%4)) continue;
-					if(visited[nr][nc]) continue;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-					visited[nr][nc] = true;
-					answer++;
-					q.add(new Node(nr,nc));
-				}
-			}
-			if(++time==l) return;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-		int T = Integer.parseInt(br.readLine());
-		for(t=1; t<=T; t++) {
-			//input
-			answer=1;
-			StringTokenizer tk = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(tk.nextToken());
-			m = Integer.parseInt(tk.nextToken());
-		    sr = Integer.parseInt(tk.nextToken());
-			sc = Integer.parseInt(tk.nextToken());
-			l = Integer.parseInt(tk.nextToken());
-			
-			graph = new int[n][m]; 
-			visited = new boolean[n][m];
-		
-			for(int i=0; i<n; i++) {
-				tk = new StringTokenizer(br.readLine());
-				for(int j=0; j<m; j++) {
-					graph[i][j] = Integer.parseInt(tk.nextToken());
-				}
-			}
-			
-			//solve
-			bfs();
+            isVisited = new boolean[N][M];
+            result = 1; // 시작 위치도 방문 처리
+            bfs(R, C);
+            sb.append("#").append(t).append(" ").append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-			//output
-			bw.write("#" + t+ " " + answer +'\n');
-		}
-		bw.close();	
-	}
+    private static void bfs(int R, int C) {
+        Queue<Pipe> q = new ArrayDeque<>();
+        q.offer(new Pipe(R, C));
+        isVisited[R][C] = true;
+        int time = 1;
+
+        while (!q.isEmpty() && time < L) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                Pipe curPipe = q.poll();
+                int x = curPipe.x;
+                int y = curPipe.y;
+                int curPipeType = map[x][y];
+
+                for (int dir : pipeDir[curPipeType]) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                    if (map[nx][ny] == 0 || isVisited[nx][ny]) continue;
+
+                    int nextPipeType = map[nx][ny];
+                    // 다음 파이프가 현재 파이프와 연결될 수 있는지 확인
+                    if (canGo(nx, ny, dir)) {
+                        isVisited[nx][ny] = true;
+                        q.offer(new Pipe(nx, ny));
+                        result++;
+                    }
+                }
+            }
+            time++;
+        }
+    }
+
+    private static boolean canGo(int x, int y, int dir) {
+        int nextPipeType = map[x][y];
+        // 현재 방향의 반대 방향으로 다음 파이프가 연결될 수 있는지 확인
+        int reverseDir = (dir % 2 == 0) ? dir + 1 : dir - 1;
+        for (int nextDir : pipeDir[nextPipeType]) {
+            if (nextDir == reverseDir) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
