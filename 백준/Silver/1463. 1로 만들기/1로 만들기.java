830c0b81f91e38074277a1b5f89d66910a4207ad
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-09-04 21:27:40 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 16868 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int[] arr;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int X = sc.nextInt();
-		
-		arr = new int[X+1];
-		System.out.println(f(X));
-	}
-	private static int f(int n) {
-		if (n==1) return 0;
-		if(arr[n]!=0) return arr[n];
-		
-		int min = Integer.MAX_VALUE;
-		
-		if (n%3 == 0) min = Math.min(min, f(n/3)+1);
-		if (n%2 == 0) min = Math.min(min, f(n/2)+1);
-		min = Math.min(min, f(n-1)+1);
-		return arr[n] = min;
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+
+        int[] arr = new int[X+1];
+
+        arr[0] = 1;
+        for (int i=2; i<=X; i++) {
+            int min = Integer.MAX_VALUE;
+
+            if (i % 3==0) min = Math.min(arr[i/3]+1, min);
+            if (i % 2==0) min = Math.min(arr[i/2]+1, min);
+            min = Math.min(arr[i-1]+1, min);
+
+            arr[i] = min;
+        }
+        System.out.println(arr[X]);
+    }
 }
