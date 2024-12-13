e9d4fe0ef8bdeee78a52db73a872d39bbf4c97c9
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-12-13 11:46:11 +0900

    Update 튜플.java

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -12,12 +12,15 @@ class Solution {
         
         // 3. , 기준 split해서 set에 집어넣기
         List<Integer> answer = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
         for(String word : words){
             String[] ws = word.split(",");
             for(String w : ws){
                 int num = Integer.parseInt(w);
-                // List에 숫자 없으면 추가
-                if (!answer.contains(num)) answer.add(num);
+                // // List에 숫자 없으면 추가
+                // if (!answer.contains(num)) answer.add(num);
+                // set에 없는 애만 추가
+                if (seen.add(num)) answer.add(num);
             }
         }
         
@@ -25,3 +28,10 @@ class Solution {
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+/* 3에서 중복있는지 확인하는 과정 
+    if(!answer.contains(num)) -> 순차탐색 O(n) : n과 함께 너무 커짐
+
+    HashSet의 add 메서드는 중복될 때 false를 반환
+    -> O(1) 시간복잡도
+    */
